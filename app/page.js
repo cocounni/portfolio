a4a8c7b5ba4a06447fbb053362d3116d7fef5753
cocounni <gpwjd0919@sookmyung.ac.kr>
2024-11-26 22:00:53 +0900

    ✨feat: top bar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
     <div className="main">
-      PORTFOLIO
+      <p>아아아</p>
     </div>
   );
 }
