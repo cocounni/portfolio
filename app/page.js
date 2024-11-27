0e4947069558c6cae0656314385eee4e86b634ff
cocounni <gpwjd0919@sookmyung.ac.kr>
2024-11-28 00:49:19 +0900

    ✨feat: About Me page excluding personal info

--- app/page.js
+++ app/page.js
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import "./globals.css";
+import { getImageSize } from "next/dist/server/image-optimizer";
 
 export default function Home() {
   return (
-    <div className="main">
-      <div className="scrollText">
-        &gt;&gt; 스크롤해서 자세히 살펴보기 &lt;&lt;
+    <div className="content">
+      <div className="main">
+        <div className="scrollText">
+          &gt;&gt; 스크롤해서 자세히 살펴보기 &lt;&lt;
+        </div>
+        <div className="HyeJung">
+          HYEJUNG <br></br>
+          PORTFOLIO
+        </div>
       </div>
-      <div className="HyeJung">
-        HYEJUNG <br></br>
-        PORTFOLIO
-      </div>
+      <div className="aboutMeContent">
+        <div className="titleAboutMe">About Me</div>
+        <img src="/avatar.png" className="img" alt="Avatar" />
+        </div>
     </div>
   );
 }
