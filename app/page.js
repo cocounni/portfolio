00047c47dd9fc15d393c2376ece37fc4aa1521a4
cocounni <gpwjd0919@sookmyung.ac.kr>
2024-11-27 23:06:58 +0900

    ✨feat: 메인 페이지 구성 및 CSS 추가

--- app/page.js
+++ app/page.js
@@ -4,6 +4,13 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="main">
+      <div className="scrollText">
+        &gt;&gt; 스크롤해서 자세히 살펴보기 &lt;&lt;
+      </div>
+      <div className="HyeJung">
+        HYEJUNG <br></br>
+        PORTFOLIO
+      </div>
     </div>
   );
 }
